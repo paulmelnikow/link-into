916c992acfcc119e60c4a7cd170b5f43e80c17fe
Paul Melnikow <email@paulmelnikow.com>
2018-11-16 02:34:24 -0500

    Run eslint

--- cli.js
+++ cli.js
@@ -2,22 +2,22 @@
 
 /* eslint no-console: "off" */
 
-'use strict';
+'use strict'
 
-const linkInto = require('./link-into');
+const linkInto = require('./link-into')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 if (! args.length) {
-  console.log('Usage: link-into destination source-pattern ...');
-  process.exit(2);
+  console.log('Usage: link-into destination source-pattern ...')
+  process.exit(2)
 }
 
 const destination = args.shift(),
-  sourcePatterns = args;
+  sourcePatterns = args
 
 linkInto(destination, sourcePatterns)
   .catch(err => {
-    console.log(err.stack);
-    process.exit(1);
-  });
+    console.log(err.stack)
+    process.exit(1)
+  })
--- link-into.js
+++ link-into.js
@@ -1,38 +1,38 @@
-'use strict';
+'use strict'
 
 const promisify = require('es6-promisify'),
   symlink = promisify(require('fs').symlink),
   mkdirp = promisify(require('mkdirp')),
   glob = promisify(require('glob')),
   path = require('path'),
-  _ = require('underscore');
+  _ = require('underscore')
 
 const createLink = (target, linkPath) => {
-  const linkDir = path.dirname(linkPath);
+  const linkDir = path.dirname(linkPath)
 
-  return mkdirp(linkDir).then(() => symlink(target, linkPath));
-};
+  return mkdirp(linkDir).then(() => symlink(target, linkPath))
+}
 
 const linkFilesMatchingPattern = (destination, sourcePattern) =>
   glob(sourcePattern)
     .then(files => Promise.all(files.map(file => {
-      const linkPath = path.join(destination, file);
+      const linkPath = path.join(destination, file)
 
       // Interpret patterns relative to cwd.
-      const relative = path.relative(path.dirname(linkPath), process.cwd());
-      const linkPointsTo = path.join(relative, file);
+      const relative = path.relative(path.dirname(linkPath), process.cwd())
+      const linkPointsTo = path.join(relative, file)
 
-      return createLink(linkPointsTo, linkPath);
-    })));
+      return createLink(linkPointsTo, linkPath)
+    })))
 
 const linkInto = (destination, sourcePatterns) => {
-  const patternIsAbsolute = _(sourcePatterns).map(path.isAbsolute);
+  const patternIsAbsolute = _(sourcePatterns).map(path.isAbsolute)
   if (_(patternIsAbsolute).some()) {
-    return Promise.reject(Error('Source patterns should be relative'));
+    return Promise.reject(Error('Source patterns should be relative'))
   }
 
   return Promise.all(sourcePatterns.map(sourcePattern =>
     linkFilesMatchingPattern(destination, sourcePattern)
-  ));
-};
-module.exports = linkInto;
+  ))
+}
+module.exports = linkInto
